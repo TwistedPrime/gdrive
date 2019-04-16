d39a799be97092634994b10f8e443839623a6f19
Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
2019-04-16 20:42:57 +0100

    Replace Google Drive API credentials with my own

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "1087538392002-dlo61j5cdlhtcnis8l7s3i617736tq3p.apps.googleusercontent.com"
+const ClientSecret = "XIsfxZ82SdFtyPU0hTbiAjx9"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
